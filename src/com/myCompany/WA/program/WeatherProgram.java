bf1a248126757f9ddeb87c6d4cb1343f78e165c1
spring-zip <spring_zip@mail.ru>
2022-02-13 15:44:19 +0300

    Seventh Version

--- src/com/myCompany/WA/program/WeatherProgram.java
+++ src/com/myCompany/WA/program/WeatherProgram.java
@@ -1,7 +1,6 @@
 package com.myCompany.WA.program;
 
 import com.myCompany.WA.controller.Controller;
-<<<<<<< HEAD
 import com.myCompany.WA.domain.NoNetworkException;
 import com.myCompany.WA.repository.CitiesRepository;
 import com.myCompany.WA.view.WeatherView;
@@ -16,38 +15,19 @@ public class WeatherProgram {
         WeatherView view = new WeatherView();
         Controller controller = new Controller();
         CitiesRepository listCities = new CitiesRepository();
-=======
-import com.myCompany.WA.repository.CitiesRepository;
-import com.myCompany.WA.view.ClientCenterView;
-
-public class WeatherProgram {
-
-    final String EXIT = "finish";
-    final String HISTORY = "list";
-
-    public void start() {
-
-        ClientCenterView view = new ClientCenterView();
-        Controller controller = new Controller();
-        CitiesRepository cities = new CitiesRepository();
-
-
->>>>>>> origin/master
 
         while (true) {
             view.start();
 
             if (controller.inputData().hasNextInt()) {
                 int historyNumber = controller.inputData().nextInt() - 1;
-<<<<<<< HEAD
+
                 try {
                     view.showCityWeather(controller.getCityByIndex(historyNumber, listCities));
                 } catch (NoNetworkException e) {
                     System.out.println(e.getMessage());
                 }
-=======
-                view.showCityInfoByHistoryNumber(historyNumber, cities);
->>>>>>> origin/master
+
             } else {
                 String inputString = controller.inputData().next();
 
@@ -56,19 +36,15 @@ public class WeatherProgram {
                 }
 
                 if (inputString.equals(HISTORY))  {
-<<<<<<< HEAD
+
                     view.showHistoryRequestCities(listCities);
+
                 } else {
                     try {
                         view.showCityWeather(controller.getCityByName(inputString, listCities));
                     } catch (NoNetworkException e) {
                         System.out.println(e.getMessage());
                     }
-=======
-                    view.showHistoryRequestCities(cities);
-                } else {
-                    view.showCityInfo(controller.getIndexCityByName(inputString, cities), cities);
->>>>>>> origin/master
                 }
             }
         }
