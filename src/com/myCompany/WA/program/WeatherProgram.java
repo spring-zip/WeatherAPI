491e9ad2339ca3d337cad656a39408722cc9a2a3
spring-zip <spring_zip@mail.ru>
2022-02-21 23:06:18 +0300

    add Class for Command Switch

--- src/com/myCompany/WA/program/WeatherProgram.java
+++ src/com/myCompany/WA/program/WeatherProgram.java
@@ -4,8 +4,6 @@ import com.myCompany.WA.domain.WeatherProgramException;
 import com.myCompany.WA.repository.CitiesRepository;
 import com.myCompany.WA.view.WeatherView;
 
-import java.util.Scanner;
-
 public class WeatherProgram {
 
     private static final String EXIT = "finish";
@@ -15,30 +13,23 @@ public class WeatherProgram {
     CitiesRepository repository = new CitiesRepository();
     WeatherView view = new WeatherView();
 
+
     public void start() {
 
         while (true) {
             view.start();
 
-            String inputData = parseData(view.inputData());
+            InputData inputData = new InputData(view.inputData());
+            String input = inputData.getCommand();
 
-            if (inputData.equals(EXIT)) {
-                break;
-            }
-
-            switch (inputData) {
-                case HISTORY_NUMBER -> getCityByHistoryNumber(view.inputData().nextInt() - 1);
+            switch (input) {
+                case EXIT -> {
+                    return;
+                }
+                case HISTORY_NUMBER -> getCityByHistoryNumber(inputData.getHistoryNumber() - 1);
                 case HISTORY -> view.showHistoryRequestCities(repository.getListCities());
-                default -> getCityByName(inputData);
+                default -> getCityByName(input);
             }
-        }
-    }
-
-    private String parseData(Scanner input) {
-        if (input.hasNextInt()){
-            return HISTORY_NUMBER;
-        } else {
-            return input.next();
         }
     }
 
